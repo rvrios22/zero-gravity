3a0635375da4733d392ab0d3a82597325c8ccf9d
Rafael Rios <rvrios22@gmail.com>
2023-10-31 16:55:33 -0700

    add: routing, pages, and components for index

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,34 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
+import Root from "./root";
+import ErrorPage from "./pages/ErrorPage";
+import Index from "./pages";
+import Gallery from "./pages/Gallery";
+import Contact from "./pages/Contact";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    children: [
+      { index: true, element: <Index /> },
+      {
+        path: '/gallery',
+        element: <Gallery />
+      },
+      {
+        path: '/contact',
+        element: <Contact />
+      }
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
---
+++ src/pages/Index.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Landing from "../components/Landing";
+import Carousel from "../components/Carousel";
+import Rating from "../components/Rating";
+
+function Index() {
+  return (
+    <>
+      <Landing />
+      <Carousel />
+      <Rating />
+    </>
+  );
+}
+
+export default Index;
