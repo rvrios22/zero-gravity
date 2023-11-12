b6a1b4d4e11b0856fe6ab01737a17698e0ef08dd
Rafael Rios <rvrios22@gmail.com>
2023-11-12 07:58:08 -0800

    add: carousel functionality. Started functions to cache data

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,42 +1,70 @@
 import React, { useEffect, useState } from "react";
-import { db } from "../config";
-import { collection, getDocs, query } from "firebase/firestore";
+import KeyboardArrowLeftIcon from "@mui/icons-material/KeyboardArrowLeft";
+import KeyboardArrowRightIcon from "@mui/icons-material/KeyboardArrowRight";
 import "../css/indexPage.css";
 
-function Carousel() {
-  const [carouselData, setCarouselData] = useState([]);
+function Carousel({ carouselData }) {
+  const [currentSlide, setCurrentSlide] = useState(0);
+  const [carouselDataCache, setCarouselDataCache] = useState({});
+  const [imageSources, setImagesSources] = useState([]);
 
-  const fetchCarouselData = async () => {
-    const carouselDataQuery = query(collection(db, "carouselImages"));
-    try {
-      const carouselDataSnapshot = await getDocs(carouselDataQuery);
-      const filteredCarouselData = carouselDataSnapshot.docs.map((doc) => ({
-        ...doc.data(),
-        id: doc.id,
-      }));
-      setCarouselData(filteredCarouselData);
-    } catch (err) {
-      console.error("error: ", err);
-    }
-  };
 
   useEffect(() => {
-    fetchCarouselData();
-  }, []);
+    carouselData.forEach((src) => {
+      const img = new Image();
+      img.src = src;
+      setCarouselDataCache((prevCache) => ({ ...prevCache, [src]: img }));
+    }, []);
+    return () => {
+      Object.values(carouselDataCache).forEach((img) =>
+        URL.revokeObjectURL(img.src)
+      );
+    };
+  }, [carouselData, setCarouselDataCache]);
 
+  const nextSlide = () => {
+    setCurrentSlide(
+      currentSlide === carouselData.length - 1 ? 0 : currentSlide + 1
+    );
+  };
+
+  const prevSlide = () => {
+    setCurrentSlide(
+      currentSlide === 0 ? carouselData.length - 1 : currentSlide - 1
+    );
+  };
+
+  console.log(carouselDataCache)
   return (
     <main>
       <h2 className="carousel-header">About Me</h2>
-      {carouselData.map((data) => (
-        <div key={data.id} className="carousel-grid-container">
-          <div className="carousel-image-conatiner">
-            <img src={data.source} alt={data.alt} />
+      <div className="carousel-flex-container">
+        {carouselData.map((data, idx) => (
+          <div
+            key={data.id}
+            className={idx === currentSlide ? "slide active" : "slide"}
+          >
+            {idx === currentSlide && (
+              <img
+                className="carousel-image"
+                src={data.source}
+                alt={data.alt}
+              />
+            )}
+            {idx === currentSlide && (
+              <p className="carousel-text">{data.desc}</p>
+            )}
           </div>
-          <div className="carousel-text-container">
-            <p>{data.desc}</p>
-          </div>
-        </div>
-      ))}
+        ))}
+      </div>
+      <div className="arrow-container">
+        <button onClick={prevSlide}>
+          <KeyboardArrowLeftIcon />
+        </button>
+        <button onClick={nextSlide}>
+          <KeyboardArrowRightIcon />
+        </button>
+      </div>
     </main>
   );
 }
--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,14 +1,34 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { db } from "../config";
+import { collection, getDocs, query } from "firebase/firestore";
 import Landing from "../components/Landing";
 import Carousel from "../components/Carousel";
 import Rating from "../components/Rating";
-import { useLoaderData } from "react-router-dom";
 
 function Index() {
+  const [carouselData, setCarouselData] = useState([]);
+
+  const fetchCarouselData = async () => {
+    const carouselDataQuery = query(collection(db, "carouselImages"));
+    try {
+      const carouselDataSnapshot = await getDocs(carouselDataQuery);
+      const filteredCarouselData = carouselDataSnapshot.docs.map((doc) => ({
+        ...doc.data(),
+        id: doc.id,
+      }));
+      setCarouselData(filteredCarouselData);
+    } catch (err) {
+      console.error("error: ", err);
+    }
+  };
+
+  useEffect(() => {
+    fetchCarouselData();
+  }, []);
   return (
     <>
       <Landing />
-      <Carousel />
+      <Carousel carouselData={carouselData}/>
       <Rating />
     </>
   );
